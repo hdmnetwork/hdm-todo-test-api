f94dc5ce451198af4cc9e6b9777baac214405f81
Fisnik Duraku <fisnik.duraku@student.vinci.be>
2025-01-22 16:56:07 +0100

    SaveTask works

--- src/Controllers/TaskController.ts
+++ src/Controllers/TaskController.ts
@@ -11,6 +11,7 @@ import DeleteTask from '../UseCase/DeleteTask/DeleteTask';
 import GetAllTasksUseCase from '../UseCase/GetAllTasks/GetAllTasksUseCase';
 import SaveTaskDto from '../UseCase/SaveTask/SaveTaskDto';
 import UseCaseFactory from '../UseCase/UseCaseFactory';
+import SaveTaskUseCase from 'src/UseCase/SaveTask/SaveTaskUseCase';
 
 @Controller()
 export default class TaskController {
@@ -23,7 +24,8 @@ export default class TaskController {
 
   @Post('/tasks')
   async create(@Body() dto: SaveTaskDto) {
-    // @todo YOU MUST FOLLOW THE SAME IMPLEMENTATION AS OTHER ENDPOINTS
+    return (await this.useCaseFactory.create(SaveTaskUseCase)).handle(dto);
+  
   }
 
   @Patch('/tasks/:id')
--- src/UseCase/SaveTask/SaveTaskUseCase.ts
+++ src/UseCase/SaveTask/SaveTaskUseCase.ts
@@ -1,17 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Task } from '@prisma/client';
 import { UseCase } from '../../index';
-import SaveTaskDto from './SaveTaskDto';
+import TaskRepository from '../../Repositories/TaskRepository';
+import SaveTaskDto from '../SaveTask/SaveTaskDto';
 
 @Injectable()
-export default class SaveTaskUseCase implements UseCase<Promise<Task>, [dto: SaveTaskDto]> {
-  constructor() {}
+export default class SaveTaskUseCase
+  implements UseCase<Promise<Task>, [dto: SaveTaskDto]>
+{
+  constructor(private readonly taskRepository: TaskRepository) {}
 
-  async handle(dto: SaveTaskDto) {
-    /*
-    * @todo IMPLEMENT HERE : VALIDATION DTO, DATA SAVING, ERROR CATCHING
-     */
+  async handle(dto: SaveTaskDto): Promise<Task> {
+    try {
+      if (!dto.name || dto.name.trim() === '') {
+        throw new BadRequestException('The "name" field is required.');
+      }
 
-    return null;
+      const task = await this.taskRepository.save({
+        id: dto.id || undefined, 
+        name: dto.name,
+        description: dto.description ?? null, 
+      });
+
+      return task;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 }
